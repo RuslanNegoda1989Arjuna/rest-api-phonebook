b013e41113a9a2861b31e302531d53ec46f836b8
Ruslan Negoda <103960102+RuslanNegoda1989Arjuna@users.noreply.github.com>
2023-03-08 16:46:19 +0200

    finish

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,6 +1,6 @@
 const express = require('express');
 
-const { validateBody, authenticate } = require("../../middlewares");
+const { validateBody, authenticate, upload } = require("../../middlewares");
 const { schemas } = require("../../models/user");
 
 // const ctrl = require("../../controllers/auth");
@@ -15,7 +15,11 @@ router.post('/register', validateBody(schemas.registerSchema), ctrl.register);
 // singin
 router.post('/login', validateBody(schemas.loginSchema), ctrl.login);
 
+
 router.get("/current", authenticate, ctrl.getCurrent);
 router.post("/logout", authenticate, ctrl.logout);
+
+// маршрут для аватарки
+router.patch("/avatars", authenticate, upload.single("avatar"), ctrl.updateAvatar)
 
 module.exports = router;
