649ca3eceda7a04af9ea7b2d137f498fe18e96ac
Ruslan Negoda <103960102+RuslanNegoda1989Arjuna@users.noreply.github.com>
2023-03-13 21:13:55 +0200

    finish

--- controllers/auth/updateAvatar.js
+++ controllers/auth/updateAvatar.js
@@ -1,6 +1,7 @@
 const { User } = require('../../models/user');
 const path = require("path");
 const fs = require("fs/promises");
+const jimp = require("jimp");
 
 const avatarsDir = path.join(__dirname, "../", "../", "public", "avatars");
 
@@ -10,6 +11,10 @@ const updateAvatar = async (req, res) => {
     const { path: tempUpload, originalname } = req.file;
     const filename = `${_id}_${originalname}`
     const resultUpload = path.join(avatarsDir, filename);
+
+    const img = await jimp.read(tempUpload);
+    await img.autocrop().cover(250, 250, jimp.HORIZONTAL_ALIGN_CENTER || jimp.VERTICAL_ALIGN_MIDDLE).writeAsync(tempUpload);
+
     await fs.rename(tempUpload, resultUpload);
     const avatarURL = path.join("avatars", filename);
     await User.findByIdAndUpdate(_id, { avatarURL });
